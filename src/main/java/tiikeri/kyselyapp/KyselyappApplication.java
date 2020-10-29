099a4be5c3df4dccec98469d2cf92e3bb3c29872
Tuukka Mattila <tuukka.mattila@myy.haaga-helia.fi>
2020-10-29 10:05:48 +0200

    Testdata in progress

--- src/main/java/tiikeri/kyselyapp/KyselyappApplication.java
+++ src/main/java/tiikeri/kyselyapp/KyselyappApplication.java
@@ -1,7 +1,14 @@
 package tiikeri.kyselyapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import tiikeri.kyselyapp.domain.Question;
+import tiikeri.kyselyapp.domain.QuestionRepository;
+import tiikeri.kyselyapp.domain.Questionnaire;
+import tiikeri.kyselyapp.domain.QuestionnaireReposiroty;
 
 @SpringBootApplication
 public class KyselyappApplication {
@@ -10,4 +17,19 @@ public class KyselyappApplication {
 		SpringApplication.run(KyselyappApplication.class, args);
 	}
 
+	
+	@Bean
+	public static CommandLineRunner demo(QuestionRepository questionRepository, QuestionnaireRepository questionnaireRepository) {
+		return (args) -> {
+			
+			Question question1 = new Question("Trivia", "What is the capital of France?", "Yes", null);
+			Question question2 = new Question("Trivia", "How long do bears hibernate?", "Yes", null);
+			
+			questionRepository.save(question1);
+			questionRepository.save(question2);
+			
+			Questionnaire questionnaire = new Questionnaire()
+			
+		};
+	}
 }
