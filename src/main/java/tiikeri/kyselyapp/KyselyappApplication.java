79cc0e25b4bc951b18ebaa761024f0d8c2ba440b
Samuli Peltonen <samuli.peltonen1@gmail.com>
2020-10-29 10:51:22 +0200

    add controller, delete logger for now

--- src/main/java/tiikeri/kyselyapp/KyselyappApplication.java
+++ src/main/java/tiikeri/kyselyapp/KyselyappApplication.java
@@ -3,13 +3,11 @@ package tiikeri.kyselyapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class KyselyappApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(KyselyappApplication.class);
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(KyselyappApplication.class, args);
---
+++ src/main/java/tiikeri/kyselyapp/web/QuestionnaireController.java
@@ -0,0 +1,33 @@
+package tiikeri.kyselyapp.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import tiikeri.kyselyapp.domain.QuestionRepository;
+import tiikeri.kyselyapp.domain.QuestionnaireRepository;
+
+@Controller
+public class QuestionnaireController {
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private QuestionnaireRepository questionnaireRepository;
+	
+	@GetMapping("/questionnaires")
+	public String indexPage(Model model) {
+		model.addAttribute("questionnaires", questionnaireRepository.findAll());
+		return "questionnaireList";
+	}
+	
+	@GetMapping("/questionnaire/{id}")
+	public String findById(@PathVariable("id") Long questionnaireId, Model model) {
+		model.addAttribute("questions", questionRepository.findAll());
+		return "questionnaire";
+	}
+	
+
+}
