b4a70823e9f5032ecb81fd7b215065f9ace38b0b
Tuukka Mattila <tuukka.mattila@myy.haaga-helia.fi>
2020-10-29 10:57:39 +0200

    Test data

--- src/main/java/tiikeri/kyselyapp/KyselyappApplication.java
+++ src/main/java/tiikeri/kyselyapp/KyselyappApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 import tiikeri.kyselyapp.domain.Question;
 import tiikeri.kyselyapp.domain.QuestionRepository;
 import tiikeri.kyselyapp.domain.Questionnaire;
-import tiikeri.kyselyapp.domain.QuestionnaireReposiroty;
+
+import tiikeri.kyselyapp.domain.QuestionnaireRepository;
+
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,18 +26,32 @@ public class KyselyappApplication {
 		SpringApplication.run(KyselyappApplication.class, args);
 	}
 
-	
 	@Bean
-	public static CommandLineRunner demo(QuestionRepository questionRepository, QuestionnaireRepository questionnaireRepository) {
+	public static CommandLineRunner demo(QuestionRepository questionRepository,
+			QuestionnaireRepository questionnaireRepository) {
 		return (args) -> {
-			
-			Question question1 = new Question("Trivia", "What is the capital of France?", "Yes", null);
-			Question question2 = new Question("Trivia", "How long do bears hibernate?", "Yes", null);
-			
+
+			Question question1 = new Question("text", "What is the capital of France?",true, null);
+			Question question2 = new Question("text", "How long do bears hibernate?", true, null);
+
 			questionRepository.save(question1);
 			questionRepository.save(question2);
+
+			Questionnaire newQuestionnaire = new Questionnaire("Trivia");
+			List<Question> questions = questionRepository.findByType("text");
+			newQuestionnaire.setQuestions(questions);
+
+			questionnaireRepository.save(newQuestionnaire);
+
+			log.info("Fetch all questions");
+			for (Question question : questionRepository.findAll()) {
+				log.info(question.toString());
+			}
 			
-			Questionnaire questionnaire = new Questionnaire()
+			log.info("Fetch all questionaires");
+			for (Questionnaire questionnaire : questionnaireRepository.findAll()) {
+				log.info(newQuestionnaire.toString());
+			}
 			
 		};
 	}
