ef339cf91dbbb117f894d890f5e12bd7ccec07c7
Samuli Peltonen <samuli.peltonen1@gmail.com>
2020-11-03 13:51:28 +0200

    Add methods save- and newQuestionnaire to QuestionnaireController

--- src/main/java/tiikeri/kyselyapp/domain/QuestionRepository.java
+++ src/main/java/tiikeri/kyselyapp/domain/QuestionRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface QuestionRepository extends CrudRepository<Question, Long> {
 	
 	public List<Question> findByType(String type);
-
+	public List<Question> findByQuestionnaire(Questionnaire questionnaire);
+ 
 }
--- src/main/java/tiikeri/kyselyapp/web/QuestionnaireController.java
+++ src/main/java/tiikeri/kyselyapp/web/QuestionnaireController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import tiikeri.kyselyapp.domain.Question;
 import tiikeri.kyselyapp.domain.QuestionRepository;
+import tiikeri.kyselyapp.domain.Questionnaire;
 import tiikeri.kyselyapp.domain.QuestionnaireRepository;
 
 @Controller
@@ -29,5 +32,17 @@ public class QuestionnaireController {
 		return "questionnaire";
 	}
 	
-
+	@RequestMapping("/newQuestionnaire")
+	public String newQuestionnaire(Model model) {
+		model.addAttribute("questionnaire", new Questionnaire());
+		return "newquestionnaire";
+		
+	}
+	
+	@RequestMapping("/save")
+	public String save(Questionnaire questionnaire, Question question) {
+		questionnaireRepository.save(questionnaire);
+		return "questionnaires";
+	}
+	
 }
