790464708026002eb4bc1f28953484a69441f23d
janimaenpaa <itsmayd@gmail.com>
2020-11-10 13:37:45 +0200

    Add CORS to Controllers

--- src/main/java/tiikeri/kyselyapp/web/QuestionnaireController.java
+++ src/main/java/tiikeri/kyselyapp/web/QuestionnaireController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import tiikeri.kyselyapp.domain.QuestionRepository;
 import tiikeri.kyselyapp.domain.Questionnaire;
 import tiikeri.kyselyapp.domain.QuestionnaireRepository;
 
+@CrossOrigin
 @Controller
 public class QuestionnaireController {
 	@Autowired
