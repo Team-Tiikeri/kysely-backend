18d0a6f2b1ad3a0b02ad7c212a6d3d89ea3975fb
janimaenpaa <itsmayd@gmail.com>
2020-11-10 14:09:20 +0200

    Add AnswerRepository

---
+++ src/main/java/tiikeri/kyselyapp/domain/AnswerRepository.java
@@ -0,0 +1,11 @@
+package tiikeri.kyselyapp.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnswerRepository extends CrudRepository<Answer, Long>{
+
+    public List<Answer> findByQuestion(Question question);
+    
+}
