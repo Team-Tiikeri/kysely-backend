ffcbe4252df212d31a75a30053d2093276c14701
Samuli Peltonen <samuli.peltonen1@gmail.com>
2020-11-26 10:14:24 +0200

    Added working methods to OptionController

--- src/main/java/tiikeri/kyselyapp/domain/OptionRepository.java
+++ src/main/java/tiikeri/kyselyapp/domain/OptionRepository.java
@@ -1,8 +1,10 @@
 package tiikeri.kyselyapp.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface OptionRepository extends CrudRepository<Option, Long> {
-
+	public List<Option> findByQuestion(Question question);
 	
 }
--- src/main/java/tiikeri/kyselyapp/web/OptionController.java
+++ src/main/java/tiikeri/kyselyapp/web/OptionController.java
@@ -1,18 +1,27 @@
 package tiikeri.kyselyapp.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import tiikeri.kyselyapp.domain.AnswerRepository;
 import tiikeri.kyselyapp.domain.Option;
 import tiikeri.kyselyapp.domain.OptionRepository;
+import tiikeri.kyselyapp.domain.Question;
 import tiikeri.kyselyapp.domain.QuestionRepository;
 
 @CrossOrigin
-@Controller
+@RestController
 public class OptionController {
 
 	@Autowired
@@ -20,17 +29,32 @@ public class OptionController {
 
 	@Autowired
 	private OptionRepository optionRepository;
-
-	@PostMapping("/saveoption")
-	public String saveOption(Option option) {
-		optionRepository.save(option);
-		return "redirect:../newquestion";
+	
+	@GetMapping("/api/questions/{id}/options")
+	public  @ResponseBody List<Option> findOptionsByQuestion(@PathVariable("id") Long questionId){
+		Question question = questionRepository.findById(questionId).orElse(null);
+		return (List<Option>) optionRepository.findByQuestion(question);
+	}
+	
+	@GetMapping("/api/options")
+	public @ResponseBody List<Option> findAllOptions(){
+		return (List<Option>) optionRepository.findAll();
+	}
+	
+	//method returns list of options for each question that has radiobutton/multiple choice
+	@PostMapping("/api/options")
+	public List<Option> saveOption(@RequestBody List<Option> options) {
+		List <Option> savedOptions = new ArrayList<Option>();
+		for (int i = 0; i < options.size(); i++) {
+			Question question = questionRepository.findById(options.get(i).getQuestion().getQuestionId()).orElse(null);
+			Option option = new Option(options.get(i).getContent(), question);
+			optionRepository.save(option);
+			savedOptions.add(option);
+		}
+	return savedOptions;
+		
 	}
 
-	@GetMapping("/newoption")
-	public String newOption(Model model) {
-		model.addAttribute("option", new Option());
-		return "newquestion";
-	}
+
 
 }
